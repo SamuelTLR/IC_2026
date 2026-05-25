dbf9aa74960799ae420ed22f9cb02dd4e7f66c22
stribeiro1 <stribeiro1@latam.stefanini.com>
2026-05-25 15:29:44 -0300

    🚧Started Windows XP

--- src/levels/Menu.jsx
+++ src/levels/Menu.jsx
@@ -1,20 +1,30 @@
 import Button from '../components/Button.jsx';
 import "./levels.css";
-import FundoEletrico from "../components/FundoEletrico.jsx";
 
 function Menu() {
     return (
         <>
-            <FundoEletrico/>
-            <div className="title-container">
-                <h1>
-                    Computer Bits
-                </h1>
-                <h2>CEFET-MG - Samuel Tomazela - 2026</h2>
-            </div>
+            <div className='windows-xp-container'>
+                <div className='windows-xp'>
+                    <div className='windows-bar'>
+                        <span className="bar-text-xp">Configuração do Computador</span>
+                        <div className="bar-controls-xp">
+                            <button className="control-btn-xp min-max">_</button>
+                            <button className="control-btn-xp min-max">□</button>
+                            <button className="control-btn-xp close">X</button>
+                        </div>
+                    </div>
+                    <div className='menu-content-xp'>
+                        <div className="welcome-text-xp">
+                            <h1>Computer Bits</h1>
+                            <h2>CEFET-MG • Samuel Tomazela • 2026</h2>
+                        </div>
 
-            <div className='main-button-container'>
-                <Button text="Iniciar" path="/levels/level-1/introduction" variant='btn_menu'/>
+                        <div className='main-button-container'>
+                            <Button text="Iniciar" path="/levels/level-1/introduction" variant='btn_menu'/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
